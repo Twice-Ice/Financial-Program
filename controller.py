00b64e725e1c3e00506eeefac700e25927fd8c77
Twice_Ice <125768114+Twice-Ice@users.noreply.github.com>
2024-05-01 10:46:49 -0600

    Misc changes
    Set up a better code structure for handling everything.
    
    Started Deposit function, it's working but won't distribute properly for containers INTO accounts.
    
    Started Create function, works for accounts, but not for containers because those need to be set up more.
    
    Need to set up a function or way to edit self.acctList in the Container class.
    
    Set up self.printContainers which prints the containers and then the accounts for the controller instance.

--- controller.py
+++ controller.py
@@ -1,26 +1,16 @@
 from moneyClasses import Account, Container
+import os
 
-def printContainers(item):
-	returnStr = ""
-	if type(item) == Account:
-		returnStr += f"{item.name}, "
-	elif type(item) == Container:
-		# returnStr += "["
-		for container in item.containerList:
-			returnStr += f"{printContainers(container)}"
-		# returnStr += "]"
-	
-	return f"{returnStr}"
 		
 class ControllerInstance:
 	def __init__(self):
-		self.total = Container("Total")
-		self.total.newAccount(Account("testAccount!"))
-		self.total.newAccount(Account("testAccount!!"))
-		self.total.newAccount(Account("testAccount!!!!"))
+		self.acctList = []
+		self.contList = []
+		self.contList.append(Container("Total"))
 
 	def update(self):
-		command = input("What would you like to do?\n")
+		command = input("\n\nWhat would you like to do?\n")
+		os.system("cls")
 		match str.lower(command):
 			case "help":
 				self.help()
@@ -37,29 +27,57 @@ class ControllerInstance:
 			case _:
 				print(f"{command} is not a valid option! Type help to see all available commands.")
 
+	def printContainers(self):
+		#prints each container as one line, and then the next line as each account with commas in between each instance.
+		contStr = ""
+		for container in self.contList:
+			contStr += (f"{container.name}, ")
+		print(f"Containers: {contStr[:len(contStr)-2]}")
+		acctStr = ""
+		for account in self.acctList:
+			acctStr += (f"{account.name}, ")
+		print(f"Accounts: {acctStr[:len(acctStr)-2]}") # cuts off the end ", "
+
 	def help(self):
 		pass
 
 	def deposit(self):
 		print("Which account would you like to deposit into?")
-		print(f"{printContainers(self.total)}\n")
-		accountName = input()
-		nameList = []
-		for container in self.total.containerList:
-			nameList.append(container.name)
-		try:
-			self.total.containerList[nameList.index(accountName)].changeVal(input("How much would you like to deposit?"))
-		except:
-			print(f"{accountName} is not a valid option")
+		self.printContainers()
+		name = str.lower(input("\n"))
+		
+		#sets up lists to see if the chosen name is actually valid.
+		contNames = []
+		for cont in self.contList:
+			contNames.append(str.lower(cont.name))
+		acctNames = []
+		for acct in self.acctList:
+			acctNames.append(str.lower(acct.name))
+
+		#if the name is valid, then a value is added to the container named as so.
+		if name in contNames:
+			self.contList[contNames.index(name)].addVal(input(f"How much would you like to deposit into {name}?\n"))
+		elif name in acctNames:
+			self.acctList[acctNames.index(name)].addVal(input(f"How much would you like to deposit into {name}?\n"))
+		else:
+			print(f"{name} is not a valid option")
 
 	def withdraw(self):
 		pass
 
 	def create(self):
-		pass
+		command = input("Would you like to create an account or a container?\n")
+		match str.lower(command):
+			case "account":
+				self.acctList.append(Account(input("What would you like to call this account?\n")))
+			case "container":
+				self.contList.append(Container(input("What would you like to call this container?\n")))
+				#need to set up getting the percentages and chosen accounts selectable as options.
+			case _:
+				print(f"I'm sorry, {command} is not an option.")
 
 	def display(self):
 		pass
 
 	def temp(self):
-		print(printContainers(self.total))
+		self.printContainers()
--- moneyClasses.py
+++ moneyClasses.py
@@ -18,23 +18,44 @@ class Account:
 		# 	print(f"{percent} is not a valid value, please input numbers only.")
 		# 	self.setPercentage()
 
-	def changeVal(self, change):
-		self.val += change
+	def addVal(self, change):
+		self.val += int(change)
 
 	def printVal(self):
 		return str(self.val)
 	
 class Container:
-	def __init__(self, name : str = "None", containerList : list = []):
+	def __init__(self, name : str = "None", acctList : list = []):
 		self.name = name
-		self.containerList = containerList #since accounts and containers should be treated very similarly, they are held within the same container list
+		self.acctList = acctList #should contain a 2d list of [name, percent]
 
-	def addVal(self, change):
+	def addVal(self, change, accts : list):
+		change = int(change)
+		# if the change isn't negative
 		if change >= 0:
-			for account in self.accountsList:
-				account.changeVal(change*account.percent)
+			#define all the names of accounts for later use
+			namesList = []
+			for account in accts:
+				namesList.append(account.name)
+
+			#checks all of the names in this container's list of names + percentages.
+			for i in range(len(self.acctList)):
+				name = self.acctList[0]
+				percent = self.acctList[1]
+				if name in namesList:
+					#if the name is in the list (all of these names should be), then the account there 
+	 				#should have it's value changed based on change and the account's percentage in this container.
+					accts[accts.index(name)].addVal(change * percent)
+				else:
+					#just in case
+					raise NameError(f"{name} isn't in the accounts list! :(")
+
+			#returns accts so that the values are all updated
+			return accts
 		else:
 			raise ValueError(f"Change, ({change}), must be larger than 0. Money can only be removed form accounts individually.")
+		
+	#need a function to edit self.acctList
 		
 	def printVal(self):
 		totalVal = 0
@@ -49,4 +70,4 @@ class Container:
 		return totalPercent
 	
 	def newAccount(self, account):
-		self.containerList.append(account)
+		self.acctList.append(account)
